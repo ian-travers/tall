b074cf0d0b07d0ee1f5ce97350a4a015b078b561
ian-travers <mr.itpe@gmail.com>
2021-03-02 15:23:23 +0300

    Two modals on one page (account settings)

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,5 +1,178 @@
 <x-layouts.user-settings username="{{ $user->username }}" title="{{ $title }}">
-    <div class="bg-white">
-        {{ $user->username }}
+    <div class="overflow-hidden rounded sm:rounded-md">
+        <div class="px-4 py-5 bg-white space-y-4">
+            <div class="border border-indigo-100 rounded sm:rounded-md">
+                <div class="bg-indigo-100 px-4 py-2">
+                    <h3 class="text-2xl sm:text-3xl">{{ __('Password') }}</h3>
+                </div>
+                <div class="px-4 py-2">
+                    <p class="my-3">{{ __('Click the button below for changing password.') }}</p>
+                    <button
+                        type="button"
+                        data-target="changePassword"
+                        class="modal-open inline-flex items-center px-4 py-2 bg-indigo-400 border border-transparent rounded-md text-white hover:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:border-indigo-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+                        {{ __('Change password') }}...
+                    </button>
+                </div>
+            </div>
+
+            <div class="border border-red-100 rounded sm:rounded-md">
+                <div class="bg-red-100 px-4 py-2">
+                    <h3 class="text-2xl sm:text-3xl">{{ __('Delete account') }}</h3>
+                </div>
+                <div class="px-4 py-2">
+                    <p class="my-3">{{ __('Once you delete your account, there is no going back. Please be certain.') }}</p>
+                    <button
+                        type="button"
+                        data-target="deleteAccount"
+                        class="modal-open inline-flex items-center px-4 py-2 bg-red-400 border border-transparent rounded-md text-white hover:bg-red-700 active:bg-red-900 focus:outline-none focus:border-red-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+                        {{ __('Delete your account') }}...
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
+
+
+    <!--Modal change password -->
+    <div
+        id="changePassword"
+        class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
+        <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+        <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+            <div
+                class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+                <svg class="fill-current text-white" width="18" height="18" viewBox="0 0 18 18">
+                    <path
+                        d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                </svg>
+                <span class="text-sm">(Esc)</span>
+            </div>
+
+            <!-- Add margin if you want to see some of the overlay behind the modal-->
+            <div class="modal-content py-4 text-left px-6">
+                <!--Title-->
+                <div class="flex justify-between items-center pb-3">
+                    <p class="text-2xl">{{ __('Changing password') }}</p>
+                    <div class="modal-close cursor-pointer z-50">
+                        <svg class="fill-current text-black" width="18" height="18" viewBox="0 0 18 18">
+                            <path
+                                d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                        </svg>
+                    </div>
+                </div>
+
+                <!--Body-->
+                <p>Modal content can go here</p>
+                <p>...</p>
+
+                <!--Footer-->
+                <div class="flex justify-end pt-2">
+                    <button
+                        class="px-4 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2">
+                        Action
+                    </button>
+                    <button class="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400">Close
+                    </button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!--Modal delete account -->
+    <div
+        id="deleteAccount"
+        class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
+        <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+        <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+            <div
+                class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+                <svg class="fill-current text-white" width="18" height="18" viewBox="0 0 18 18">
+                    <path
+                        d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                </svg>
+                <span class="text-sm">(Esc)</span>
+            </div>
+
+            <!-- Add margin if you want to see some of the overlay behind the modal-->
+            <div class="modal-content py-4 text-left px-6">
+                <!--Title-->
+                <div class="flex justify-between items-center pb-3">
+                    <p class="text-2xl">{{ __('Deleting account') }}</p>
+                    <div class="modal-close cursor-pointer z-50">
+                        <svg class="fill-current text-black" width="18" height="18" viewBox="0 0 18 18">
+                            <path
+                                d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                        </svg>
+                    </div>
+                </div>
+
+                <!--Body-->
+                <p>Modal content can go here</p>
+                <p>...</p>
+
+                <!--Footer-->
+                <div class="flex justify-end pt-2">
+                    <button
+                        class="px-4 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2">
+                        Action
+                    </button>
+                    <button class="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400">Close
+                    </button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    @push('scripts')
+        <script>
+            var openmodal = document.querySelectorAll('.modal-open')
+            let selectedModalTargetId = ''
+            for (var i = 0; i < openmodal.length; i++) {
+                openmodal[i].addEventListener('click', function (event) {
+                    selectedModalTargetId = event.target.attributes.getNamedItem('data-target').value
+                    event.preventDefault()
+                    toggleModal()
+                })
+            }
+
+            var overlay = document.querySelectorAll('.modal-overlay')
+            for (var i = 0; i < overlay.length; i++) {
+                overlay[i].addEventListener('click', toggleModal)
+            }
+
+
+            var closemodal = document.querySelectorAll('.modal-close')
+            for (var i = 0; i < closemodal.length; i++) {
+                closemodal[i].addEventListener('click', toggleModal)
+            }
+
+            document.onkeydown = function (evt) {
+                evt = evt || window.event
+                var isEscape = false
+                if ("key" in evt) {
+                    isEscape = (evt.key === "Escape" || evt.key === "Esc")
+                } else {
+                    isEscape = (evt.keyCode === 27)
+                }
+                if (isEscape && document.body.classList.contains('modal-active')) {
+                    toggleModal()
+                }
+            }
+
+            function toggleModal() {
+                if (!selectedModalTargetId) {
+                    return
+                }
+                const body = document.querySelector('body')
+                const modal = document.getElementById(selectedModalTargetId)
+                modal.classList.toggle('opacity-0')
+                modal.classList.toggle('pointer-events-none')
+                body.classList.toggle('modal-active')
+            }
+        </script>
+    @endpush
+
 </x-layouts.user-settings>
