e0955ed647a804d993470cbda4e485ae01efa800
ian-travers <mr.itpe@gmail.com>
2021-03-16 09:14:31 +0300

    IDE editor js intends are changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,18 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-        colors: {
-            'nfsu-brand': '#003548'
-        },
-        backgroundImage: theme => ({
-            'nfsu-map': "url('/storage/map-d50.png');",
-        })
+      colors: {
+        'nfsu-brand': '#003548'
+      },
+      backgroundImage: theme => ({
+        'nfsu-map': "url('/storage/map-d50.png');",
+      })
     },
   },
   variants: {
     extend: {},
   },
   plugins: [
-      require('@tailwindcss/forms'),
+    require('@tailwindcss/forms'),
   ],
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,10 @@
 const mix = require('laravel-mix');
 
 mix
-    .setPublicPath('public/build')
-    .setResourceRoot('/build/')
-    .js('resources/js/app.js', 'js')
-    .postCss('resources/css/app.css', 'css', [
-        require("tailwindcss"),
-    ])
-    .version();
+  .setPublicPath('public/build')
+  .setResourceRoot('/build/')
+  .js('resources/js/app.js', 'js')
+  .postCss('resources/css/app.css', 'css', [
+    require("tailwindcss"),
+  ])
+  .version();
